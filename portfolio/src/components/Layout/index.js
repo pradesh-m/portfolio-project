5a72849238891642cfa532549b4afa59024f9066
Pradesh Mainali <pradeshm10@gmail.com>
2024-01-03 17:49:18 -0500

    Created layout component. Added sidebar component to layout. Adjusted color scheme. Added routing from components/layout to App.js.

---
+++ portfolio/src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import Sidebar from '../Sidebar';
+import './index.scss';
+
+const Layout = () => {
+    return <>
+    <Sidebar/>
+    </>
+}
+
+export default Layout
+
+
+//creating general pattern for all components index files
---
+++ portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,6 @@
+import './index.scss';
+
+const Sidebar = () => <>Hello from Sidebar</>
+
+export default Sidebar
+
