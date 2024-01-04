034cf531e1dea1d77601cf8d9c2952d45099968e
Pradesh Mainali <pradeshm10@gmail.com>
2024-01-03 19:02:36 -0500

    Turned navigation bar into a header.

--- portfolio/src/components/Sidebar/index.js
+++ portfolio/src/components/Sidebar/index.js
@@ -1,6 +1,25 @@
 import './index.scss';
+import {Link} from 'react-router-dom'
+import Logo from '../../assets/images/logo.png'
 
-const Sidebar = () => <>Hello from Sidebar</>
+const Sidebar = () => (
+
+// creating a navigation bar. 
+// logo link will take to '/' aka homepage
+// 
+<div className = 'navigation-bar'>
+
+    <Link className = 'logo' to= '/'>
+        <img src = {Logo} alt = 'logo'/>
+    </Link>
+
+</div>
+
+
+
+)
+
+
 
 export default Sidebar
 
