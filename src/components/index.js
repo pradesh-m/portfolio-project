a6f9f02ab4e89648416e5e6e8d90d0c0e1cbfc19
Pradesh Mainali <pradeshm10@gmail.com>
2024-01-04 19:56:49 -0500

    Working on NavBar

--- src/components/index.js
+++ src/components/index.js
@@ -5,8 +5,10 @@ import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
 import Works from './Works';
-import Feedbacks from './Feedbacks';
 import Contact from './Contact';
+
+// the css for all of the components
+
 
 export {
   Hero,
@@ -15,7 +17,6 @@ export {
   Tech,
   Experience,
   Works,
-  Feedbacks,
   Contact,
   EarthCanvas, 
   BallCanvas, 
