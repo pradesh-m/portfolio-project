b9724b05a5a8ee0b6244caf39ddc8db5f3170387
Pradesh Mainali <pradeshm10@gmail.com>
2024-01-13 21:04:17 -0500

    Setting up deployment.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "https://pradesh-m.github.io/portfolio-project/",
+  base : "/portfolio-project/",
   plugins: [react()],
 })
