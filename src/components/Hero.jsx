7a3b189b3fcb4ada7c1b2f8957b55e8fb6fa3851
Pradesh Mainali <pradeshm10@gmail.com>
2024-01-22 17:52:41 -0500

    fixing image styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,32 +19,28 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-10 text-white-100`}>
             Outside of programming, I enjoy outdoor activities and recreational sports. I've
              been wrestling for five years and like to play pick-up sports with friends. 
-          </p>
-          <p className={`${styles.heroSubText} mt-10 text-white-100`}>
-            Take a look at some of my projects, and feel free to reach out!           
-          </p>
+          </p>      
         </div>
       </div>
 
       {/* Image and Logos Row */}
 
-      <div className={`relative inset-0 top-[600px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+      <div className={`relative inset-0 top-[500px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         {/* Image */}
         <div>
-        <img src={imageOfSelf} alt="PersonalPicture" className="shadow-glow w-80 h-[25vh]" />
+        <img src={imageOfSelf} alt="PersonalPicture" className="shadow-glow w-80 h-[30vh]" />
         </div>
 
         {/* Social Icons Column */}
         <div className='flex flex-col items-center justify-between'>
-
           {/* LinkedIn Icon */}
           <a href="https://www.linkedin.com/in/pradesh-mainali-02ab0b264/" target="_blank" rel="noopener noreferrer" className="mb-4">
-            <img src={linkedinLogo} alt="LinkedIn" className="w-20 mt-0 h-20" />
+            <img src={linkedinLogo} alt="LinkedIn" className="w-20 mt-0 h-[8vh]" />
           </a>
 
           {/* GitHub Icon */}
           <a href="https://github.com/pradesh-m" target="_blank" rel="noopener noreferrer">
-            <img src={githubLogo} alt="GitHub" className="w-20 mt-0 h-20" />
+            <img src={githubLogo} alt="GitHub" className="w-20 mt-0 h-[8vh]" />
           </a>
         </div>
       </div>
